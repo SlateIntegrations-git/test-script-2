c7de8818c52755d1cfd9178660bb2b0c6f348ee9
jake-slate-git <jacob@slateintegrations.com>
2026-05-21 07:51:13 -0700

    Update test-script-2.sh

--- test-script-2.sh
+++ test-script-2.sh
@@ -5,3 +5,8 @@ ping -c 8 1.1.1.1
 echo "############"
 echo "PINGS COMPLETE"
 echo "############"
+
+curl -O https://raw.githubusercontent.com/SlateIntegrations-git/test-script-2/main/test-script-3.sh
+
+chmod +x test-script-3.sh
+./test-script-3.sh
